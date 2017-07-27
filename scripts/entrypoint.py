3d4e6f6147cdca0ef153ef601ed28043649dad5a
Thiago <thiago.pinto@quintoandar.com.br>
2017-07-27 18:50:50 -0300

    Added convertion to number function.

--- scripts/entrypoint.py
+++ scripts/entrypoint.py
@@ -4,10 +4,24 @@ import sys
 import json
 import requests
 
+
+def convertToNumber(job):
+    convertedJob = {}
+    for k, v in job.iteritems():
+        if isinstance(v, dict):
+            convertedJob[k] = convertToNumber(v)
+        elif '.number' in k:
+            convertedJob[k.split('.')[0]] = int(v)
+        else:
+            convertedJob[k] = v
+    return convertedJob
+
+
 if __name__ == "__main__":
     config = json.loads(sys.argv[2]).get("vargs")
     server = config.get("server")
-    job_config = config.get("job_config")
+    job_config = convertToNumber(config.get("job_config"))
+    print job_config
 
     r = requests.get(('%s/v1/jobs/%s') % (server, job_config.get("id")))
     if r.status_code == 200:
