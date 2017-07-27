9000bf8390b5f023578afd55e0c0ce597da0780c
Thiago <thiago.pinto@quintoandar.com.br>
2017-07-27 20:06:55 -0300

    Fix creation route

--- scripts/entrypoint.py
+++ scripts/entrypoint.py
@@ -4,19 +4,20 @@ import sys
 import json
 import requests
 
-if __name__ == "__main__":
-    config = json.loads(sys.argv[2]).get("vargs")
-    server = config.get("server")
-    job_config = config.get("job_config")
+if __name__ == '__main__':
+    config = json.loads(sys.argv[2]).get('vargs')
+    server = config.get('server')
+    job_config = config.get('job_config')
 
     print job_config
 
-    r = requests.get(('%s/v1/jobs/%s') % (server, job_config.get("id")))
+    r = requests.get(('%s/v1/jobs/%s') % (server, job_config.get('id')))
     if r.status_code == 200:
         print 'Updating job'
-        r = requests.put('%s/v0/scheduled-jobs/%s' % (server, job_config.get("id")), json=job_config)
+        r = requests.put('%s/v0/scheduled-jobs/%s' % (server, job_config.get('id')), json=job_config)
     else:
         print 'Creating new job'
-        r = requests.post('%s/v0/scheduled-jobs/' % (server), json=job_config)
+        r = requests.post('%s/v0/scheduled-jobs' % (server), json=job_config)
 
-    print str(r)
+    print 'Status code:' + str(r.status_code)
+    print 'Body: ' + r.text
