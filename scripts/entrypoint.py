e9c8f2ec8c7b61ae5e746abc06fcc0733dfaa51b
Thiago <thiago.pinto@quintoandar.com.br>
2017-07-31 13:51:44 -0300

    Using recursive mpa update

--- scripts/entrypoint.py
+++ scripts/entrypoint.py
@@ -3,6 +3,16 @@
 import sys
 import json
 import requests
+import collections
+
+def update(d, u):
+    for k, v in u.iteritems():
+        if isinstance(v, collections.Mapping):
+            r = update(d.get(k, {}), v)
+            d[k] = r
+        else:
+            d[k] = u[k]
+    return d
 
 if __name__ == '__main__':
     config = json.loads(sys.argv[2]).get('vargs')
@@ -13,9 +23,8 @@ if __name__ == '__main__':
     r = requests.get(('%s/v1/jobs/%s') % (server, job_config.get('id')))
     if r.status_code == 200:
         print 'Updating job'
-        updated_job = json.loads(r.text)
-        updated_job.update(job_config)
-        r = requests.put('%s/v0/scheduled-jobs/%s' % (server, updated_job.get('id')), json=updated_job)
+        job_config = update(job_config, json.loads(r.text))
+        r = requests.put('%s/v0/scheduled-jobs/%s' % (server, job_config.get('id')), json=job_config)
     else:
         print 'Creating new job'
         r = requests.post('%s/v0/scheduled-jobs' % (server), json=job_config)
