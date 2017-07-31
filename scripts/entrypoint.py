94e0e4c56c2f584f8ce900cbe638840a38bffeb1
Thiago <thiago.pinto@quintoandar.com.br>
2017-07-31 12:09:52 -0300

    Fixed updated order for existing jobs.

--- scripts/entrypoint.py
+++ scripts/entrypoint.py
@@ -8,14 +8,14 @@ if __name__ == '__main__':
     config = json.loads(sys.argv[2]).get('vargs')
     server = config.get('server')
     job_config = config.get('job_config')
-
     print job_config
 
     r = requests.get(('%s/v1/jobs/%s') % (server, job_config.get('id')))
     if r.status_code == 200:
         print 'Updating job'
-        job_config.update(json.loads(r.text))
-        r = requests.put('%s/v0/scheduled-jobs/%s' % (server, job_config.get('id')), json=job_config)
+        updated_job = json.loads(r.text)
+        updated_job.update(job_config)
+        r = requests.put('%s/v0/scheduled-jobs/%s' % (server, updated_job.get('id')), json=updated_job)
     else:
         print 'Creating new job'
         r = requests.post('%s/v0/scheduled-jobs' % (server), json=job_config)
